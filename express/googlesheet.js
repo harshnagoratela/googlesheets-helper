69dd382b98d4b454a418e6e89102f4b003d8976e
Harshna Goratela <harshna.goratela@gmail.com>
2020-09-16 18:34:49 +0530

    register user function

--- express/googlesheet.js
+++ express/googlesheet.js
@@ -46,6 +46,37 @@ const addCommentsToGS = async (sheetId, client_email, private_key, newData ) => 
   }
 }
 
+const registerUserToGS = async (sheetId, client_email, private_key, newRequest ) => {
+  
+  try {	  
+	    
+	  private_key = private_key.split("\\n").join("\n")
+	  const doc = new GoogleSpreadsheet(sheetId);
+	  // use service account creds
+	  await doc.useServiceAccountAuth({
+		client_email,
+		private_key
+	  });
+	  await doc.loadInfo(); // loads document properties and worksheets
+
+	  const sheet = doc.sheetsByTitle["Users"];  
+	  
+	  const jsonData = JSON.parse(newRequest);
+	  
+	  await sheet.addRow({
+		"email": jsonData.email,
+		"password": jsonData.password,
+		"fullname": jsonData.fullname,
+		"isActive": jsonData.isActive,
+		"creationdate": jsonData.creationdate		
+	  });
+	  return true;
+  } catch (error) {
+	  console.log(error);
+	return error;
+  }
+}
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Welcome to Google Sheets Helper via Netlify Functions!</h1>');
@@ -62,7 +93,25 @@ router.post('/addcomment', async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'POST')
   
   //console.log("*** request.body.privateKey ",req.body.privateKey);
-  const result = await addCommentsToGS(req.body.sheetId,req.body.clientEmail,req.body.privateKey,req.body.newData);
+  const result = await addCommentsToGS(req.body.sheetId,req.body.clientEmail,req.body.privateKey,req.body.newRequest);
+  //const result = true;
+  res.json({
+    result
+  })
+});
+
+router.get('/registeruser', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>This serverless function will add a comment to googlesheets!</h1>');
+  res.end();
+});
+router.post('/registeruser', async (req, res) => {
+  //cors related changes
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST')
+  
+  //console.log("*** request.body.privateKey ",req.body.privateKey);
+  const result = await registerUserToGS(req.body.sheetId,req.body.clientEmail,req.body.privateKey,req.body.newRequest);
   //const result = true;
   res.json({
     result
@@ -73,6 +122,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use('/.netlify/functions/googlesheet', router);  // path must route to lambda
 app.use('/.netlify/functions/googlesheet/addcomment', router);  // path must route to lambda
+app.use('/.netlify/functions/googlesheet/registeruser', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
